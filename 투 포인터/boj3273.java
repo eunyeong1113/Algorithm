7b0e67d0eb2d9a84d4e9e9d5c77f9d865034848d
eunyeong1113 <eylee1113@naver.com>
2023-08-05 01:43:01 +0900

    230805 투포인터

---
+++ 투 포인터/boj3273.java
@@ -0,0 +1,47 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj3273 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		Arrays.sort(arr);
+		
+		int start=0;
+		int end=N-1;
+		int cnt=0;
+		int sum=0;
+		
+		while(start<end) {
+			sum=arr[start]+arr[end];
+			if(sum==num) {
+				cnt++;
+				start++;
+				end--;
+			}
+			else if(sum<num) {
+				start++;
+			}
+			else if(sum>num) {
+				end--;
+			}
+		}
+		System.out.println(cnt);
+
+	}
+
+}
